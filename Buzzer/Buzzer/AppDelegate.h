da258cd62d02d916d9a510b0e92c07c397a3a74b
scotasaurus <scotmci@Scots-iMac.local>
2014-07-29 12:31:28 -0700

    Initial Project Checkin
    Skeleton Project

---
+++ Buzzer/Buzzer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Buzzer
+//
+//  Created by Scot McIntosh on 7/29/14.
+//  Copyright (c) 2014 ___SKUNKd___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Buzzer/Buzzer/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Buzzer
+//
+//  Created by Scot McIntosh on 7/29/14.
+//  Copyright (c) 2014 ___SKUNKd___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
