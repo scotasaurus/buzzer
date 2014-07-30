3c47d8f4815ee75fd8dd01d4c0644d5ced20be15
manndras <rdaly@rdalyiMac.local>
2014-07-30 16:21:13 -0700

    Revert 3366b72..f7de2d5
    This rolls back to commit 3366b7240e463cbf66d026f26aa682eb6c18215f.

---
+++ BuzzeriOS/Buzzer/BZZTextField.h
@@ -0,0 +1,13 @@
+//
+//  BZZTextField.h
+//  Buzzer
+//
+//  Created by rdaly on 7/30/14.
+//  Copyright (c) 2014 ___SKUNKd___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BZZTextField : UITextField
+
+@end
