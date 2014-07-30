96145bd2e23c119af786df0353a145570454aff4
scotasaurus <scotmci@Scots-iMac.local>
2014-07-30 12:07:55 -0700

    Initial checkin of device methods
    Web service calls for registering and managing device state.

---
+++ BuzzeriOS/Buzzer/Model/DeviceRequest.m
@@ -0,0 +1,61 @@
+//
+//  DeviceRequest.m
+//  Buzzer
+//
+//  Created by Scot McIntosh on 7/30/14.
+//  Copyright (c) 2014 ___SKUNKd___. All rights reserved.
+//
+
+#import "DeviceRequest.h"
+
+static NSString * oneParameterApi   = @"%@%@/%@";
+static NSString * threeParameterApi = @"%@%@/%@/%@/%@";
+
+
+@implementation DeviceRequest
+
+- (void)registerDeviceAsync:(NSString *)deviceName withDeviceOwner:(NSString *)deviceOwner andDeviceInfo:(NSString *)deviceInfo {
+    NSString *urlString = [[NSString alloc] initWithFormat:threeParameterApi, baseUrl, registerDeviceRequestUrl, deviceName, deviceOwner, deviceInfo];
+    [super makeRequest:urlString];
+}
+
+- (void)getDeviceAsync:(NSString *)deviceId {
+    NSString *urlString = [[NSString alloc] initWithFormat:oneParameterApi, baseUrl, getDeviceRequestUrl, deviceId];
+    [super makeRequest:urlString];
+}
+
+- (void)isSignaledAsync:(NSString *)deviceId {
+    NSString *urlString = [[NSString alloc] initWithFormat:oneParameterApi, baseUrl, isSignaledRequestUrl, deviceId];
+    [super makeRequest:urlString];
+}
+
+- (void)removeDeviceAsync:(NSString *)deviceId {
+    NSString *urlString = [[NSString alloc] initWithFormat:oneParameterApi, baseUrl, removeDeviceRequestUrl, deviceId];
+    [super makeRequest:urlString];
+}
+
+- (Device *)getDevice:(NSString *)deviceId {
+    Device * device = nil;
+    
+    NSDictionary * jsonData = [self getDataFromResponse];
+    if (jsonData != nil)
+    {
+        NSDictionary * deviceData = jsonData[@"device"];
+        device = [self _parseDeviceDataFromDictionary:deviceData];
+    }
+    
+    return device;
+}
+
+// TODO: Make this static and allow meetings controller to use this.
+- (Device *)_parseDeviceDataFromDictionary:(NSDictionary *)deviceData {
+    Device * device = [[Device alloc] initDevicewithId:deviceData[@"id"]
+                                               andName:deviceData[@"name"]
+                                               andInfo:deviceData[@"deviceInfo"]
+                                              andOwner:deviceData[@"deviceOwner"]
+                                             andStream:deviceData[@"stream"]];
+    
+    return device;
+}
+
+@end
